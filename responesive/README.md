503a075aec911ea492afd7e57d3054268acfaa8f
dinhhoang382 <hoang382.wk@gmail.com>
2024-10-09 21:21:41 +0700

    Responsive Login Form & Test

---
+++ responesive/README.md
@@ -0,0 +1,8 @@
+# Responsive Login Form
+
+- Responsive Login Form Using HTML CSS & JavaScript
+- Contains beautiful background image and glass style.
+- Simple input field validation.
+- With show and hide password function.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
---
+++ responesive/assets/js/main.js
@@ -0,0 +1,30 @@
+/*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPassword = (inputPassword, inputIcon) => {
+  const input = document.getElementById(inputPassword),
+        iconEye = document.getElementById(inputIcon)
+
+  iconEye.addEventListener('click', () => {
+    // Change password to text
+    if (input.type === 'password') {
+      // Switch to text
+      input.type = 'text'
+
+      // Add icon
+      iconEye.classList.add('ri-eye-line')
+
+      // Remove icon
+      iconEye.classList.remove('ri-eye-off-line')
+    } else {
+      // Change to password
+      input.type = 'password'
+
+      // Remove icon
+      iconEye.classList.remove('ri-eye-line')
+
+      // Add icon
+      iconEye.classList.add('ri-eye-off-line')
+    }
+  })
+}
+
+showHiddenPassword('password', 'input-icon')
