3465cc7d9c242b9831bb9067d5f8529e1dcc353e
mtq308 <tranminhquoc945@gmail.com>
2024-10-28 15:07:45 +0700

    Settings some modifications to fix API login bug

--- backend/PHP/MockProject-app/app/Models/Employee.php
+++ backend/PHP/MockProject-app/app/Models/Employee.php
@@ -5,25 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens; // Thêm dòng này
+use Laravel\Sanctum\HasApiTokens;
+
+// Thêm dòng này
 
 class Employee extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    
+
     protected $table = 'Employee'; // Bảng Employee
-    protected $fillable = ['name', 'email', 'username', 'password', 'role_id', 'status'];
+    protected $fillable = ['id', 'name', 'birth_date', 'email', 'phone_number', 'avatar', 'username', 'password', 'role_id', 'status'];
 
     public $timestamps = false;
-
-    // protected $fillable = [
-    //     'id', 'category_id', 'description', 'image', 'price', 'stock',
-    // ];
     protected $guarded = [];
 
     public function role()
-    {   
+    {
         return $this->belongsTo(Role::class, 'role_id');
-        // return $this->morphOne(Role::class, 'roleable');
+        return $this->morphOne(Role::class, 'roleable');
     }
 }
--- backend/PHP/MockProject-app/database/migrations/2024_10_25_132344_create_personal_access_tokens_table.php
+++ backend/PHP/MockProject-app/database/migrations/2024_10_25_132344_create_personal_access_tokens_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePersonalAccessTokensTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
+            $table->morphs('tokenable');
+            $table->string('name'); // Đảm bảo đã có cột 'name'
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable(); // Thêm cột 'abilities' nếu chưa có
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('personal_access_tokens');
     }
-};
+}
